ea6ba8540d2eefdc4be481f9c5a5eb352bab7d3c
aahmad94 <aka5vm@virginia.edu>
2017-11-22 01:18:36 -0800

    add user model

--- frontend/wookiee.jsx
+++ frontend/wookiee.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import configureStore from './store/store';
+import configureStore from './store/store';
 // import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<h1>Welcome to Wookiee</h1>, document.getElementById('root'));
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Welcome to Wookiee</h1>, root);
 });
