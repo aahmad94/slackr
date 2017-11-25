8ef69cbaf595bc4151298771f066417f98e1ee78
aahmad94 <aka5vm@virginia.edu>
2017-11-25 15:43:44 -0800

    add messagerelayjob to message model (louis)

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,10 @@
 class Message < ApplicationRecord
 
+  validates :body, presence: true
+  after_commit { MessageRelayJob.perform_later(self, self.context) }
+
   belongs_to :user
 
-  belongs_to :interface, polymorphic: true 
+  belongs_to :interface,
+    polymorphic: true
 end
