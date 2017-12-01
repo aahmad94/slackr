671fe702a3ff271eb314461274d788811ef4ce66
aahmad94 <aka5vm@virginia.edu>
2017-12-01 11:23:32 -0800

    update splash css with flex positioning

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -17,13 +17,15 @@ export default (props) => {
   if (!props.currentUser) {
     return (
       <div className="splash">
-        <img src="https://a.slack-edge.com/52353/marketing/img/home/home_illo.png" />
-        <h1>
-          Where Teams Collaborate
-        </h1>
-        <p>
-          When your team needs to kick off a project, hire a new employee, deploy some code, review a sales contract, finalize next year's budget, measure an A/B test, plan your next office opening, and more, Wookiee has you covered.        </p>
-        <a className="demo-btn" onClick={guestLogin}>Get started</a>
+      <img src="https://a.slack-edge.com/52353/marketing/img/home/home_illo.png" />
+        <div className="splash-info">
+          <h1>
+            Where Work Happens
+          </h1>
+          <p>
+            When your team needs to kick off a project, hire a new employee, deploy some code, review a sales contract, finalize next year's budget, measure an A/B test, plan your next office opening, and more, Slack has you covered.</p>
+          <a className="demo-btn" onClick={guestLogin}>Get started</a>
+        </div>
       </div>
     );
   } else {
