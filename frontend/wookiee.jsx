555fab509bf65ac423898f929bbc88898d95888c
aahmad94 <aka5vm@virginia.edu>
2017-12-03 14:35:09 -0800

    add code in wookiee entry file to ping host domain every 5 min

--- frontend/wookiee.jsx
+++ frontend/wookiee.jsx
@@ -5,6 +5,11 @@ import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
 
+  var http = require("http");
+    setInterval(function() {
+    http.get("http://wookiee.herokuapp.com");
+  }, 300000); // every 5 minutes (300000)
+
   let preloadedState = {};
 
   if (window.currentUser) {
