91bbb68c287696b7caafb997cce83d165cfaf153
aahmad94 <aka5vm@virginia.edu>
2018-02-13 00:52:54 -0800

    update favicon

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -24,7 +24,7 @@ export default (props) => {
           </h1>
           <p>
             When your team needs to kick off a project, hire a new employee, deploy some code, review a sales contract, finalize next year's budget, measure an A/B test, plan your next office opening, and more, Wookiee has you covered.</p>
-          <a className="demo-btn" onClick={guestLogin}>Demo</a>
+          <a className="demo-btn" onClick={guestLogin}>Demo Login</a>
         </div>
       </div>
     );
