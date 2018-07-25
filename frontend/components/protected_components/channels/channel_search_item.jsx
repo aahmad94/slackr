a40aa1038571cbc85e42a3f0c5dd8051ee45360c
aahmad94 <aka5vm@virginia.edu>
2018-07-25 11:20:48 -0700

    update styling for modal channel search results

--- frontend/components/protected_components/channels/channel_search_item.jsx
+++ frontend/components/protected_components/channels/channel_search_item.jsx
@@ -3,13 +3,19 @@ import React from 'react';
 export default ({ channel, addSubscriberToChannel, currentUser }) => (
   <div className='channel-search-item'>
     <div className='information'>
-      <strong>{channel.channel_name}</strong><br />
-      #{channel.channel_name}<br />
-      {channel.user_ids.length} user
+      <strong># {channel.channel_name}</strong>
+      <div>
+        {
+          channel.user_ids.length > 1 ?
+          `${channel.user_ids.length} users` : 
+          `${channel.user_ids.length} user`
+        }        
+      </div>
     </div>
     {(channel.user_ids.includes(currentUser.id)) ?
-      <button className='disabled'>Joined</button> :
-      <button className='enabled'
-        onClick={addSubscriberToChannel}>Join</button>}
+      <a className='disabled'>Joined</a> :
+      <a className='enabled'
+        onClick={addSubscriberToChannel}>Join</a>}
   </div>
 );
+                               
